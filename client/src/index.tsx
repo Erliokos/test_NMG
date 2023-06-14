79bd7af9b6966e1c496c9e23cb4d69c9017a0f86
Альберт Мурзаков <albertmurzakov@MacBook-Pro-Albert.local>
2023-06-15 01:33:40 +0300

    client init graphql, appolo, codegen

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import * as GlobalStyled from './GlobalStyle'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_DOMAIN,
+  cache: new InMemoryCache()
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <ApolloProvider client={client}>
+    <GlobalStyled.Root />
+      <App />
+  </ApolloProvider>
+)
 
 
 
